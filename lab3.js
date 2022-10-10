eeca735a762b3e228551a3bee3582c2fcd623af4
andruga225 <ryanworld225@yandex.ru>
2022-10-10 10:21:33 +0300

    fixes

--- lab3.js
+++ lab3.js
@@ -1,73 +1,127 @@
-function addRuFields(){
-    let text=document.getElementById("new-field-ru").value;
-    let parent=document.getElementById("form-ru");
+function addFields(){
+    let text=document.getElementById("new-field").value;
+    let parent=document.getElementsByClassName("form-style")[0];
     let newItem=document.createElement("input");
     newItem.placeholder=text;
 
     parent.appendChild(newItem);
 }
 
-function addEnFields(){
-    let text=document.getElementById("new-field-en").value;
-    let parent=document.getElementById("form-en");
-    let newItem=document.createElement("input");
-    newItem.placeholder=text;
+let userName="пользователь";
+let curLocal="ru";
 
-    parent.appendChild(newItem);
-}
+let local = {
+    "ru":{
+        "header":"Форма",
+        "btn-change-local":"En Version",
+        "btn-login":"Войти",
+        "btn-logout":"Выйти",
+        "hello":"Привет, ",
+        "first-field":"Первое поле:",
+        "second-field":"Второе поле:",
+        "new-field":"Плейсхолдер для нового поля:",
+        "btn-new-field":"Добавить поле",
+    },
 
-function switchToEn(){
-    document.getElementById("ru").style.display="none";
-    document.getElementById("en").style.display="block";
-}
+    "en":{
+        "header":"Form",
+        "btn-change-local":"Ru Version",
+        "btn-login":"Auth",
+        "btn-logout":"LogOut",
+        "hello":"Hello, ",
+        "first-field":"First field:",
+        "second-field":"Second field:",
+        "new-field":"Placeholder for new field:",
+        "btn-new-field":"Add field",
+    },
+};
 
-function switchToRu(){
-    document.getElementById("en").style.display="none";
-    document.getElementById("ru").style.display="block";
-}
+function localize() {
 
-function authRu() {
-    let userName = prompt('Введите Ваше имя:')
-    let validUserName = /\S/;
-    if(!validUserName.test(userName)){
-        alert("Пустое имя пользователя");
-        return;
+    if(curLocal=="ru"){
+        curLocal="en";
+        if(userName=="пользователь"){
+            userName="user";
+        }
+    }else{
+        curLocal="ru";
+        if(userName=="user"){
+            userName="пользователь";
+        }
     }
 
-    let text = document.getElementById("hello-ru");
-    text.innerHTML="Привет, "+userName+"!";
+    let elements=document
+        .querySelectorAll("[id]")
+        .forEach(translateElement)
 
-    document.getElementById("btnLoginRu").style.display="none";
-    document.getElementById("btnLogOutRu").style.display="block";
 }
 
-function logOutRu(){
-    let text = document.getElementById("hello-ru");
-    text.innerHTML="Привет, пользователь!";
+function translateElement(element) {
+    let key=element.getAttribute("id");
 
-    document.getElementById("btnLoginRu").style.display="block";
-    document.getElementById("btnLogOutRu").style.display="none";
+    if(key=="hello"){
+        element.innerHTML = local[curLocal][key] + userName + "!";
+    }else {
+        element.innerHTML = local[curLocal][key];
+    }
 }
 
-function authEn() {
-    let userName = prompt('Enter Your name:')
-    let validUserName = /\S/;
-    if(!validUserName.test(userName)){
-        alert("Empty username");
-        return;
+function auth() {
+    let mes=""
+
+    if(curLocal=="ru")
+    {
+        mes="Введите Ваше имя:"
+    }else{
+        mes="Enter your name:"
     }
 
-    let text = document.getElementById("hello-en");
-    text.innerHTML="Hello, "+userName+"!";
+    userName = prompt(mes)
 
-    document.getElementById("btnLoginEn").style.display="none";
-    document.getElementById("btnLogOutEn").style.display="block";
+    let validUserName = /\S/;
+    if(!validUserName.test(userName)){
+
+        if(curLocal=="ru") {
+            alert("Пустое имя пользователя");
+            return;
+        }
+
+        if(curLocal=="en"){
+            alert("Empty username");
+            return;
+        }
+    }
+
+    let text = document.getElementById("hello");
+
+    if(curLocal=="ru") {
+        text.innerHTML = "Привет, " + userName + "!";
+    }
+
+    if(curLocal=="en") {
+        text.innerHTML = "Hello, " + userName + "!";
+    }
+
+    document.getElementById("btn-login").style.display="none";
+    document.getElementById("btn-logout").style.display="block";
 }
 
-function logOutEn(){
-    let text = document.getElementById("hello-en");
-    text.innerHTML="Hello, user!";
+function logOut(){
+    let text = document.getElementById("hello");
 
-    document.getElementById("btnLoginEn").style.display="block";
-    document.getElementById("btnLogOutEn").style.display="none";
+    if(curLocal=="ru") {
+
+        userName="пользователь"
+        text.innerHTML="Привет, "+userName+"!";
+    }
+
+    if(curLocal=="en") {
+
+        userName="user"
+        text.innerHTML="Hello, "+userName+ "!";
+    }
+
+    document.getElementById("btn-login").style.display="block";
+    document.getElementById("btn-logout").style.display="none";
 }
+
